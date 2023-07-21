82c75525c8c97f4af374b12162618c57d5985252
Hadifathi1378 <hadifathi13781378@gmail.com>
2023-07-21 11:53:40 +0330

    setup.py modified

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
-config = {
-    'name': 'ms-mint',
-    'version': versioneer.get_version(),
-    'cmdclass': versioneer.get_cmdclass(),
-    'description': 'Metabolomics Integrator (Mint)',
-    'long_description': long_description,
-    'long_description_content_type': 'text/markdown'
-}
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='pycoin',
+    version='v1.0.01',
+    packages=['pycoin', 'pycoin/market_data_gathering', 'pycoin/market_data_kline_plots'],
+    license="MIT",
+    author='Hadi Fathipour',
+    author_email="hadi9628983@gmail.com",
+    url='https://github.com/hadif1999/pycoin',
+    description="a packege to make some algorithmic trading analyses easy.",
+    install_requires=['plotly', 'pandas', 'numpy', 'kucoin-python'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
