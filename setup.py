77c2cec96466166e20210d1b784f08246af87db4
Hadifathi1378 <hadifathi13781378@gmail.com>
2023-07-21 13:43:59 +0330

    setup.py updated

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@
 
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 
 setup(
     name='pythoncoin',
-    version='v1.0.01',
+    version='v1.0.02',
     packages=['pycoin', 'pycoin/market_data_gathering', 'pycoin/market_data_kline_plots'],
     license="MIT",
     author='Hadi Fathipour',
@@ -14,6 +18,8 @@ setup(
     url='https://github.com/hadif1999/pycoin',
     description="a packege to make some algorithmic trading analyses easy.",
     install_requires=['plotly', 'pandas', 'numpy', 'kucoin-python'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
