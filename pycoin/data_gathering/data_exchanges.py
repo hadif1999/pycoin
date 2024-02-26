47714efd1cb2d3bc35cfb87931c11f3c5ecb414c
hadi <hadifathi13781378@gmail.com>
2024-02-26 14:49:27 +0330

    data_gathering modified

--- pycoin/data_gathering/data_exchanges.py
+++ pycoin/data_gathering/data_exchanges.py
@@ -5,18 +5,18 @@ import datetime as dt
 
 
 
-def KlineData_Fetcher(symbol: str, timeframe: str, data_exchange:str|None = None,
-                      ccxt_exchange:ccxt.Exchange|None = None, 
+def KlineData_Fetcher(symbol: str, timeframe: str, data_exchange:str,
                       since: int|dt.datetime|None = None, 
                       limit:int = 1000, fill_missing: bool = True, 
-                      drop_incomplete: bool = True, datetime_index: bool = True):
+                      drop_incomplete: bool = True, datetime_index: bool = True,
+                      throw_exc: bool = False):
     
     
-    if not data_exchange and not ccxt_exchange: raise ValueError("'ccxt_exchange' or 'data_exchange' must have value")
-    if data_exchange: assert data_exchange in Data_Exchanges.keys(), f"exchange not found, current exchanges: {list(Data_Exchanges.keys())}"
-    data_fetcher = Data_Exchanges.get(data_exchange.lower(), None) or ccxt_exchange.fetch_ohlcv
-    ohlcv = data_fetcher(symbol=symbol, timeframe=timeframe,
-                         since=None, limit=limit)
+    data_exchange = data_exchange.lower() 
+    assert data_exchange in Data_Exchanges.keys(), f"exchange not found, current exchanges: {list(Data_Exchanges.keys())}"
+    data_fetcher = Data_Exchanges[data_exchange].fetch_ohlcv
+    kwargs = dict(symbol=symbol, timeframe=timeframe, limit=limit)
+    ohlcv = data_fetcher(**kwargs, since = None)
     all_ohlcv = []
     if since: 
         if isinstance(since, dt.datetime): _since_ = int(since.timestamp()*1000)
@@ -24,14 +24,16 @@ def KlineData_Fetcher(symbol: str, timeframe: str, data_exchange:str|None = None
         targetTime = ohlcv[-1][0]
         prev_last_fetched_time = None
         while True:
-            _ohlcv_ =  data_fetcher(symbol=symbol, timeframe=timeframe,
-                                    since=_since_, limit=limit)
+            try: _ohlcv_ =  data_fetcher(**kwargs, since=_since_)
+            except ccxt.BadRequest as e:
+                if throw_exc: raise e
+                print(f"\n {e}, fetching available timerange...\n")
+                _ohlcv_ = data_fetcher(**kwargs, since = None)
             last_fetched_time = _ohlcv_[-1][0]
             if last_fetched_time == prev_last_fetched_time: break
             else: 
                 _since_ = last_fetched_time
                 all_ohlcv += _ohlcv_
-                
             prev_last_fetched_time = last_fetched_time  
             print(f"""\n\n{data_exchange}|{timeframe}|{symbol}  
                   untill {utils.ts2dt(int(all_ohlcv[-1][0]/1000)).__str__()} fetched""")
@@ -40,6 +42,8 @@ def KlineData_Fetcher(symbol: str, timeframe: str, data_exchange:str|None = None
     df = postprocess_Data(ohlcv=all_ohlcv, timeframe = timeframe,
                           fill_missing=fill_missing, drop_incomplete=drop_incomplete,
                           datetime_index=datetime_index, symbol = symbol)
+    exchange_name = data_fetcher.__module__.split('.')[1]
+    df.Name = f"{symbol}_{exchange_name}_{timeframe}"
     return df
 
 
@@ -57,5 +61,4 @@ def postprocess_Data(ohlcv: list[float], fill_missing: bool = True,
      
 
 
-Data_Exchanges = {"binance": ccxt.binance().fetch_ohlcv, "bingx": ccxt.bingx().fetch_ohlcv,
-                  "kucoin": ccxt.kucoin().fetch_ohlcv, "htx": ccxt.htx().fetch_ohlcv} 
+Data_Exchanges = {exchange: getattr(ccxt, exchange)() for exchange in ccxt.exchanges}
